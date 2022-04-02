c9fb8fd1faeb7b12d1a40eb0a81fc3b80ea2041e
Padraig <20085012@mail.wit.ie>
2022-04-02 17:34:36 +0100

    SignIn Activity

--- app/src/main/java/wit/assignments/mobapp2_1/activities/SignInActivity.kt
+++ app/src/main/java/wit/assignments/mobapp2_1/activities/SignInActivity.kt
@@ -1,4 +1,17 @@
 package wit.assignments.mobapp2_1.activities
 
-class SignInActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import wit.assignments.mobapp2_1.databinding.ActivitySigninBinding
+
+
+class SignInActivity : AppCompatActivity(){
+    private lateinit var activitySigninBinding : ActivitySigninBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        activitySigninBinding = ActivitySigninBinding.inflate(layoutInflater)
+
+    }
 }
